da846ddfbced205a1c18d3e024739ac09f96f5f9
asgalan04 <147687449+asgalan04@users.noreply.github.com>
2025-03-24 19:09:20 -0700

    Add files via upload

---
+++ Playlist.java
@@ -0,0 +1,58 @@
+package model;
+import java.util.*;
+
+public class Playlist {
+	
+	private ArrayList<Song> songs;
+	private String name;
+	
+	public Playlist(String name) {
+		this.name = name;
+		songs = new ArrayList<Song>();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public ArrayList<Song> getSongs() {
+		return new ArrayList<Song>(songs);
+	}
+	
+	public void addSong(Song s) {
+		songs.add(s);
+	}
+	
+	/*
+	 * Removes a song by the title of the playlist
+	 * 
+	 */
+	
+	public boolean removeSong(String title) {
+		for (Song s : songs) {
+			if (s.getName().toLowerCase().equals(title.toLowerCase())) {
+				songs.remove(s);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public void removeFirst() {
+		songs.remove(0);
+	}
+	
+	public int getSize() {
+		return songs.size();
+	}
+	
+	public String toString() {
+		String message=""+name;
+		for(Song s:songs) {
+			message+=songs+"/n";
+		}
+		return message;
+	
+		
+	}
+}
